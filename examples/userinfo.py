35d38d34b272559b35c71ffd26ef82354f4be78c
RedBall <studiosredmotion@gmail.com>
2021-01-04 12:03:49 -0800

    Added more to userinfo ex

--- examples/userinfo.py
+++ examples/userinfo.py
@@ -3,3 +3,16 @@ import SelfDiscord
 client = SelfDiscord(token='token')
 
 print(f"Hello my discord is {client.username}#{client.discriminator}")
+print(f"You can contact me at {client.email} or {client.phone}")
+if client.nsfw_allowed:
+    print("I am above the age of 18.")
+else:
+    print("I am below the age of 18.")
+if client.verified:
+    print("My account is verified.")
+else:
+    print("My account is not verified.")
+if client.mfa_enabled:
+    print("My account has 2FA enabled.")
+else:
+    print("My account does not have 2FA enabled.")
